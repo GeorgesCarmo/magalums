24328a0aedc870be8ad4eae1e010f4e8a2de9f1a
Georges do Carmo Pereira <139582154+GeorgesCarmo@users.noreply.github.com>
2024-08-06 13:31:07 -0300

    Update class getter and setter

--- src/main/java/com/georges/magalums/entity/Notification.java
+++ src/main/java/com/georges/magalums/entity/Notification.java
@@ -17,4 +17,63 @@ public class Notification {
     private String destination;
 
     private String message;
+
+    @ManyToOne
+    @JoinColumn(name = "channel_id")
+    private Channel channel;
+
+    @ManyToOne
+    @JoinColumn(name = "status_id")
+    private Status status;
+
+    public Notification() {
+    }
+
+    public Long getNotificationId() {
+        return notificationId;
+    }
+
+    public void setNotificationId(Long notificationId) {
+        this.notificationId = notificationId;
+    }
+
+    public LocalDateTime getLocalTime() {
+        return localTime;
+    }
+
+    public void setLocalTime(LocalDateTime localTime) {
+        this.localTime = localTime;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Channel getChannel() {
+        return channel;
+    }
+
+    public void setChannel(Channel channel) {
+        this.channel = channel;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }

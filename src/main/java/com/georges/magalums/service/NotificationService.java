b3711f7838f744527ae1903eb8a4fbd74003ad70
Georges do Carmo Pereira <139582154+GeorgesCarmo@users.noreply.github.com>
2024-08-06 14:37:37 -0300

    Create service to cancelNotification by Id

--- src/main/java/com/georges/magalums/service/NotificationService.java
+++ src/main/java/com/georges/magalums/service/NotificationService.java
@@ -2,6 +2,7 @@ package com.georges.magalums.service;
 
 import com.georges.magalums.controller.dto.ScheduleNotificationDTO;
 import com.georges.magalums.entity.Notification;
+import com.georges.magalums.entity.Status;
 import com.georges.magalums.repository.NotificationRepository;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +23,14 @@ public class NotificationService {
 
     public Optional<Notification> findById(Long notificationId){
         return notificationRepository.findById(notificationId);
+    }
+
+    public void cancelNotification(Long notificationId){
+        var notification = findById(notificationId);
+
+        if (notification.isPresent()){
+            notification.get().setStatus(Status.values.CANCELED.toStatus());
+            notificationRepository.save(notification.get());
+        }
     }
 }
